52e8cc50af4dd675eeb923c4473d7e71451dedeb
alisabouri68 <alisabouri6857@gmail.com>
2025-01-16 17:13:26 +0330

    qweqwe

--- assets/scripts/SPA.js
+++ assets/scripts/SPA.js
@@ -1,45 +1,55 @@
-import router from "./router.js";
-const progres =document.querySelectorAll('.progres>div')
- function locationHandler(a) {
-
- const rout =  router[a];
- const html =  new XMLHttpRequest();
- console.log(html)
- html.open("GET", rout.template , true);
- html.onload = function () {
-     let data = html.responseText;
-     document.querySelector("#content").innerHTML =  data;
- };
- html.send();
-}
- function urlRoutes(e) {
-let xxx =  e.target.getAttribute('data-route')
-window.location.hash =  xxx
-
-  locationHandler(xxx);
-
-}
-document.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (!e.target.className.includes("link")) {
-    return false;
-  } else {
-    urlRoutes(e);
-  }
-});
-
-
-
-window.onpopstate = locationHandler;
-window.addEventListener('hashchange', locationHandler);
+import ebi from "./artist.js";
+const terend = document.querySelector('.terend')
+const progres = document.querySelectorAll('.progres>div')
 function randomHeight() {
-  let x = Math.floor(Math.random()*16)
+  let x = Math.floor(Math.random() * 16)
   return x + 1
 }
 function progresHeight() {
-  progres.forEach(span=>{
-    span.style.height=randomHeight()+"px"
+  progres.forEach(span => {
+    span.style.height = randomHeight() + "px"
   })
-  
 }
 progresHeight()
+function terends() {
+  ebi.forEach((item , i) => {
+  let div = document.createElement('div')
+  div.classList.add('flex', 'justify-around', 'items-center', 'w-full',"gap-3")
+  div.innerHTML = `
+                                <div>
+                                    <span class="text-lg font-extrabold text-black-one opacity-20">
+                                        ${i + 1}
+                                    </span>
+                                </div>
+                                <div>
+                                    <figure>
+                                        <img class="overflow-hidden object-cover rounded-2xl"
+                                            src="${item.img}" width="70" height="70" alt="">
+                                    </figure>
+                                </div>
+                                <div class="flex flex-col grow">
+                                    <div class=""><span class="text-lg text-black-one font-semibold">${item.song}</span></div>
+                                    <div><span class="text-lg text-black-one opacity-20 ">artist</span></div>
+                                </div>
+                                <div><span class="text-base text-black-one font-extrabold opacity-70">04:01</span></div>
+                                <div class="w-10 h-10 bg-red-one rounded-full flex justify-center items-center">
+                                    <svg class="w-5 h-5 text-wite-one">
+                                        <use href="#play"></use>
+                                    </svg>
+                                </div>
+                                <div>
+                                    <ul>
+                                        <li>
+                                            <span>
+                                                <svg class="size-6 text-black-one">
+                                                    <use href="#elipsis"></use>
+                                                </svg>
+                                            </span>
+                                        </li>
+                                    </ul>
+                                </div>
+  `
+  terend.appendChild(div);
+})
+}
+terends()
---
+++ assets/scripts/artist.js
@@ -0,0 +1,42 @@
+const ebi = [
+    {
+        template:"https://sv.ahoramusics.ir/iran/ebi/Ebi%20-%20Aadat.mp3",
+        song:"adat",
+        img:"https://ahoramusics.ir/wp-content/uploads/2024/01/%D8%AF%D8%A7%D9%86%D9%84%D9%88%D8%AF-%D8%AA%D9%85%D8%A7%D9%85-%D8%A2%D9%87%D9%86%DA%AF-%D9%87%D8%A7%DB%8C-%D8%A7%D8%A8%DB%8C-%D8%A7%D8%B2-%D9%82%D8%AF%DB%8C%D9%85-%D8%AA%D8%A7-%D8%AC%D8%AF%DB%8C%D8%AF-ebi.jpg"
+    },
+    {
+        template:"https://sv.ahoramusics.ir/iran/ebi/Ebi%20-%20Bi%20Etena.mp3",
+        song:"etena",
+        img:"https://ahoramusics.ir/wp-content/uploads/2024/01/%D8%AF%D8%A7%D9%86%D9%84%D9%88%D8%AF-%D8%AA%D9%85%D8%A7%D9%85-%D8%A2%D9%87%D9%86%DA%AF-%D9%87%D8%A7%DB%8C-%D8%A7%D8%A8%DB%8C-%D8%A7%D8%B2-%D9%82%D8%AF%DB%8C%D9%85-%D8%AA%D8%A7-%D8%AC%D8%AF%DB%8C%D8%AF-ebi.jpg"
+
+    },
+    {
+        template:"https://sv.ahoramusics.ir/iran/ebi/Ebi%20-%20Akhare%20Ghesseh.mp3",
+        song:"akhare_Ghese_Ebi",
+        img:"https://ahoramusics.ir/wp-content/uploads/2024/01/%D8%AF%D8%A7%D9%86%D9%84%D9%88%D8%AF-%D8%AA%D9%85%D8%A7%D9%85-%D8%A2%D9%87%D9%86%DA%AF-%D9%87%D8%A7%DB%8C-%D8%A7%D8%A8%DB%8C-%D8%A7%D8%B2-%D9%82%D8%AF%DB%8C%D9%85-%D8%AA%D8%A7-%D8%AC%D8%AF%DB%8C%D8%AF-ebi.jpg"
+
+    },
+    {
+        template:"https://sv.ahoramusics.ir/iran/ebi/Ebi%20-%20Begoo%20Ey%20Yar%20Begoo.mp3",
+        song:" Bego_Ey_Yar_Bego",
+        img:"https://ahoramusics.ir/wp-content/uploads/2024/01/%D8%AF%D8%A7%D9%86%D9%84%D9%88%D8%AF-%D8%AA%D9%85%D8%A7%D9%85-%D8%A2%D9%87%D9%86%DA%AF-%D9%87%D8%A7%DB%8C-%D8%A7%D8%A8%DB%8C-%D8%A7%D8%B2-%D9%82%D8%AF%DB%8C%D9%85-%D8%AA%D8%A7-%D8%AC%D8%AF%DB%8C%D8%AF-ebi.jpg"
+
+    },
+    {
+        template:"https://sv.ahoramusics.ir/iran/ebi/Ebi%20-%20Derakht.mp3",
+        song:"Derakht",
+        img:"https://ahoramusics.ir/wp-content/uploads/2024/01/%D8%AF%D8%A7%D9%86%D9%84%D9%88%D8%AF-%D8%AA%D9%85%D8%A7%D9%85-%D8%A2%D9%87%D9%86%DA%AF-%D9%87%D8%A7%DB%8C-%D8%A7%D8%A8%DB%8C-%D8%A7%D8%B2-%D9%82%D8%AF%DB%8C%D9%85-%D8%AA%D8%A7-%D8%AC%D8%AF%DB%8C%D8%AF-ebi.jpg"
+
+    }
+]
+
+
+export default ebi;
+
+
+
+
+
+
+
+
